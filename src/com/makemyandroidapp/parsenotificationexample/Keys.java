cbc870db2c715548d6cbf892c8b1e4bca3828480
Tim C <foamyguy@gmail.com>
2013-12-30 18:38:33 -0600

    initial commit

---
+++ src/com/makemyandroidapp/parsenotificationexample/Keys.java
@@ -0,0 +1,7 @@
+package com.makemyandroidapp.parsenotificationexample;
+
+public class Keys {
+	// Fill in your keys
+	protected static final String applicationId = "";
+	protected static final String clientKey = "";
+}
---
+++ src/com/makemyandroidapp/parsenotificationexample/ParseReceiver.java
@@ -0,0 +1,66 @@
+package com.makemyandroidapp.parsenotificationexample;
+
+import java.util.Iterator;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+public class ParseReceiver extends BroadcastReceiver {
+	private final String TAG = "Parse Notification";
+	private String msg = "";
+	@Override
+	public void onReceive(Context ctx, Intent intent) {
+		Log.i(TAG, "PUSH RECEIVED!!!");
+		
+		try {
+			String action = intent.getAction();
+			String channel = intent.getExtras().getString("com.parse.Channel");
+			JSONObject json = new JSONObject(intent.getExtras().getString(
+					"com.parse.Data"));
+
+			Log.d(TAG, "got action " + action + " on channel " + channel + " with:");
+			Iterator itr = json.keys();
+			while (itr.hasNext()) {
+				String key = (String) itr.next();
+				Log.d(TAG, "..." + key + " => " + json.getString(key));
+				if(key.equals("string")){
+					msg = json.getString(key);
+				}
+			}
+		} catch (JSONException e) {
+			Log.d(TAG, "JSONException: " + e.getMessage());
+		}
+		
+		
+		Bitmap icon = BitmapFactory.decodeResource(ctx.getResources(),
+                R.drawable.happy);
+		
+		Intent launchActivity = new Intent(ctx, MainActivity.class);
+		PendingIntent pi = PendingIntent.getActivity(ctx, 0, launchActivity, 0);
+		
+		Notification noti = new NotificationCompat.Builder(ctx)
+        .setContentTitle("PUSH RECEIVED")
+        .setContentText(msg)
+        .setSmallIcon(R.drawable.happy)
+        .setLargeIcon(icon)
+        .setContentIntent(pi)
+        .setAutoCancel(true)
+        .build();
+		
+		NotificationManager nm = (NotificationManager)ctx.getSystemService(Context.NOTIFICATION_SERVICE);
+		nm.notify(0, noti);
+
+	}
+
+}
